bda8989510416ad1b1d5499ba58e96f3ee41a3b2
forthright47 <k.tuhinjaman@gmail.com>
2025-12-03 23:19:08 +0600

    Phase 11: Add scroll up functionality

--- scripts/main.js
+++ scripts/main.js
@@ -46,3 +46,13 @@ const swiperReviews = new Swiper('.reviews-swiper', {
     desableOnInteraction: false,
   }
 });
+
+
+// Show Scroll Up
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
